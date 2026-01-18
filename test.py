0729b3c3db1700c32c95a691a73738e69a3f94b0
TiahRam <tiahrami@gmail.com>
2026-01-18 15:04:59 +0100

    starting the a_maze_ing project

---
+++ test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from typing import Dict
+import argparse
+
+
+def arg_parsing() -> Dict:
+    """ Parse all arguments variable using the argparse module
+        It doesn't take any args and return dict of all the configuration
+        variable given in the config file or empty dict if invalid args, or 
+        if failed to open the file
+    """
+
+    config_variables = {}
+    try:
+        parser = argparse.ArgumentParser()
+        parser.add_argument('config_file', help='Path to configuration file')
+        args = parser.parse_args()
+        with open(args.config_file, 'r') as f:
+            for line in f:
+                line = line.strip()
+                if not line or line.startswith('#'):
+                    continue
+                key, value = line.split("=", 1)
+                config_variables[key] = value
+    except Exception:
+        print(f"can't open file {args.config_file}, no such file or directory")
+    return config_variables
+
+
+print(arg_parsing())
