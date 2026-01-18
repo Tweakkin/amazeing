38b40f64428c00ae6f95fed2206db5e77eafaeca
Tweakkin <yahyaboukhmira7@gmail.com>
2026-01-18 18:57:28 +0100

    handled some erros

--- Tweakkin_amazing/main.py
+++ Tweakkin_amazing/main.py
@@ -1,16 +1,34 @@
 import sys
 
 if __name__ == "__main__":
-	if len(sys.argv) < 2:
-		print("Error: Missing configuration file.")
+	# sys.argv must contain exactly 2 items: [script_name, config_file]
+	if len(sys.argv) != 2:
+		print("Error: Usage is python3 main.py <config_file>")
 		sys.exit(1)
-	with open(sys.argv[1], "r") as file:
-		parsed_dict = {}
-		for line in file:
-			line = line.strip()
-			if not line[0] == '#':
+	try:
+		#using 'with' to always close the file
+		with open(sys.argv[1], "r") as file:
+			parsed_dict = {}
+			#looping through lines of the file
+			for line in file:
+				#removing white spaces at the beggining and end of file
+				line = line.strip()
+				#skipping the line if its empty
+				if not line:
+					continue
+				#skipping the line if its a comment
+				if line.startswith('#'):
+					continue
+				#splitting when first '=' is found
 				line = line.split(sep='=', maxsplit=1)
+				#adding result to the dict
 				parsed_dict.update({line[0].strip() : line[1].strip()})
-		print(parsed_dict)
-
-#testing git
+			print(parsed_dict)
+	#handling if file was not found error
+	except FileNotFoundError:
+		print(f"Error: the file '{sys.argv[1]} was not found")
+		sys.exit(1)
+	#handling permission denied error
+	except PermissionError:
+		print(f"Error: you do not have permission to read '{sys.argv[1]}'")
+		sys.exit(1)
